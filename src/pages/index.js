be97ca4c5fdfc93a186a8b04c5733802b9c1f0ed
TheBlueDude7 <51275606+TheBlueDude7@users.noreply.github.com>
2023-08-28 19:04:38 -0500

    Google 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech} from '@/components';
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 const DynamicMain = dynamic(() => import('@/components/DynamicCanvas'), {
   ssr: false,
 })
 export default function Home() {
   return (
+    <>
+    <GoogleAnalytics trackPageViews={true} TRACKING_ID={'G-GVNQ0FHHYE'} gaMeasurementId={'G-GVNQ0FHHYE'}/>
     <DynamicMain />
+    </>
   )
 }
